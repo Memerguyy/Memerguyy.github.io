cea0e831d8c3d49cc58b83bdbca3ff322c5be0bf
Memerguy <mateuszluigi2007@gmail.com>
2023-12-27 19:22:52 +0100

    :trollface:
    :trollface:

---
+++ main/script.js
@@ -0,0 +1,5 @@
+let btn = document.getElementById('btn')
+
+function alertLol(){
+    alert("Lol I'm not for hire xd I can barely move my own ass up for myself in the mornings, no way can I do the same for otehrs")
+}
