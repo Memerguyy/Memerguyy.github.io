82ffde55f2cd2aae74ff37c7e2d2bfff9b95ee2d
Memerguy <mateuszluigi2007@gmail.com>
2023-12-28 00:10:15 +0100

    a ton
    a TON

--- main/script.js
+++ main/script.js
@@ -1,5 +1,11 @@
-let btn = document.getElementById('btn')
+// if you see this, may god forgive your eyes for the shitshow that is my code, thank you, good luck
+
+let meowThing = document.getElementById("meow")
 
 function alertLol(){
-    alert("Lol I'm not for hire xd I can barely move my own ass up for myself in the mornings, no way can I do the same for otehrs")
+    alert("Lol I'm not for hire xd I can barely move my own ass up for myself in the mornings, no way can I do the same for others")
+}
+
+function startMeow() {
+    meowThing.play()
 }
