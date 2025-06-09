9d21b1f262201b55f92c4e1a4b750d9f6554c646
Memer <mateuszluigi2007@gmail.com>
2025-06-09 10:57:00 +0200

    update

--- assets/startPageAssets/main.js
+++ assets/startPageAssets/main.js
@@ -4,31 +4,24 @@ let newTitle = [];
 let positionPositive = 0;
 let positionNegative = websiteTitleArray.length
 
-
-
-window.onload = (e) => {
-    for (let i = 0; i <= websiteTitleArray.length; i++) {
-        // // console.log("AAAAAAAAAAAAA")
-        // // newTitle.push(websiteTitleArray[websiteTitleArray.length-1])
-        // // newTitle.push(websiteTitleArray[0])
-        // // websiteTitleArray.pop();
-        // // websiteTitleArray.shift();
-        // // console.log(newTitle);
-        // console.log(w
+function array_move(arr, old_index, new_index) {
+    while (old_index < 0) {
+        old_index += arr.length;
     }
-    websiteTitleArray.forEach(e => {
-        
-        // console.log(websiteTitle.substring(websiteTitle.length,position) + " " + websiteTitle.substring(position, 0))
-        
-    });
-}
+    while (new_index < 0) {
+        new_index += arr.length;
+    }
+    if (new_index >= arr.length) {
+        var k = new_index - arr.length + 1;
+        while (k--) {
+            arr.push(undefined);
+        }
+    }
+    arr.splice(new_index, 0, arr.splice(old_index, 1)[0]);
+    return arr.join(''); 
+    // im not even lying i stole this shit from npm, idc, it works, im too lazy to type allat
+};
 
 setInterval(() => {
-    document.title = websiteTitle.substring(0,positionPositive) + " " + websiteTitle.substring(positionNegative, 0)
-    positionPositive++
-    positionNegative--
-    if (positionPositive >= websiteTitleArray.length) {
-        positionNegative = websiteTitleArray.length
-        positionPositive = 0
-    }
+    document.title = array_move(websiteTitleArray,-websiteTitleArray.length,-1)
 }, 200);
